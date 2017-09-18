03643b318119ee5f00c4e3f59dca3c661fc037ff
Jigesh Thampan <jigeshthampan@gmail.com>
2017-09-18 17:48:53 +0530

    First Test Commit

---
+++ src/test/java/in/jigs/spring/bean/TestLoginService.java
@@ -0,0 +1,14 @@
+package in.jigs.spring.bean;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestLoginService {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
